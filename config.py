03c2fb1f7e9be3e6ed570c0d24e1add5ef5b67a4
Edoardo Cappelli <edoardocappelli1999@gmail.com>
2024-07-25 11:41:16 +0200

    some updates

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Variables
 target_class = 1 
 original_class = 7 
-N_iter = 100 # total number of iterations in teoria dovrebbero essere 2020
-N_1 = 100 # number of the first order iterations
+N_iter = 2020 # total number of iterations
+N_1 = 500 # number of the first order iterations
 α = 0.8 # learning rate of the first order iterations [0.8, 0.9, 1, 1.1]
 β = 0.15 # learning rate of the second order iterations
 classes = 10 # number of classes (MNIST)
--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import config
 
 def main():
     dataset = data_preparation.load_dataset()
-    x = utils.get_random_image(config.original_class, dataset, seed=111)
+    x = utils.get_random_image(config.target_class, dataset, seed=111)
     N_iter = config.N_iter
     N_1 = config.N_1
     α = config.α
     β = config.β
+    j = config.target_class
+    x_j = utils.get_random_image(config.target_class, dataset, seed=111)
+    image_to_attack = utils.get_random_image(config.original_class, dataset, seed=111)
     
-    squat.SQUAT(x, N_iter, N_1, α, β)
+    squat.SQUAT(image_to_attack, x_j, j, N_iter, N_1, α, β)
     
     # model = model_definition.define_model()
     # training.train_model(model)
