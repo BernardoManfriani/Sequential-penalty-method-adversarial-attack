1608dcdbba19a543895dc76c86cae79ecbde91d4
Edoardo Cappelli <98023490+Edo745@users.noreply.github.com>
2024-10-18 18:08:11 +0200

    Final version run.ps1

--- run.ps1
+++ run.ps1
@@ -3,6 +3,5 @@ python squat-penalty-attack.py --true-label 4 --target-label 9 --tau 1.0 --rho 1
 python squat-penalty-attack.py --true-label 3 --target-label 0 --tau 1.0 --rho 1.5 --Niter 1000
 
 # Same digits, differents hyperparameters
-python squat-penalty-attack.py --true-label 2 --target-label 6 --tau 1.0 --rho 1.1 --Niter 1000
-# python squat-penalty-attack.py --true-label 2 --target-label 6 --tau 1.0 --rho 1.5 --Niter 1000
-
+python squat-penalty-attack.py --true-label 8 --target-label 1 --tau 1.0 --rho 1.1 --Niter 1000
+python squat-penalty-attack.py --true-label 8 --target-label 1 --tau 1.0 --rho 10 --Niter 1000
